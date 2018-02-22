8885d589c20f2cb067ba9c0700581837e58bd592
Yauheni Dubouski <joinhud@gmail.com>
2018-02-22 21:06:34 +0300

    love-triangles/feature: implement method for counting love triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,30 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var triangles = [];
+  var result = 0;
+
+  for (var i = 0; i < preferences.length; i++) {
+    var firstIndex = i + 1;
+    var secondIndex = preferences[i];
+    var thirdIndex = preferences[secondIndex - 1];
+    var belovedThird = preferences[thirdIndex - 1];
+
+    if (
+      isNotInLoveTriangle(firstIndex, triangles) && 
+      isNotInLoveTriangle(secondIndex, triangles) &&
+      secondIndex !== firstIndex &&
+      thirdIndex !== firstIndex &&
+      belovedThird === firstIndex
+    ) {
+      triangles.push(firstIndex, secondIndex, thirdIndex);
+      result++;
+    }
+  }
+
+  return result;
 };
+
+function isNotInLoveTriangle(item, triangles) {
+  return triangles.indexOf(item) < 0;
+}
